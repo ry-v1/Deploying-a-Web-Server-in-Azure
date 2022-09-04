61fbfed0614abc62258366e9e27051eb806640b3
ry-v1 <72290009+ry-v1@users.noreply.github.com>
2022-09-04 17:17:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,83 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.21.1"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+  subscription_id = "XXXXX"
+  tenant_id       = "XXXX"
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-resources"
-  location = var.location
+# Create/Reuse a Resource Group.
+data "azurerm_resource_group" "main" {
+  name     = "Azuredevops"
 }
 
+# Create a Virtual network and a subnet on that virtual network.
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/22"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
+  resource_group_name  = data.azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# Create a Network Security Group. 
+# Ensure that you explicitly allow access to other VMs on the subnet 
+# and deny direct access from the internet.
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-nsg"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "AllowSubnetInBound"
+    description                = "Allow access to other VMs on the subnet"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+  security_rule {
+    name                       = "DenyInternetInBound"
+    description                = "Deny direct access from the Internet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+}
+
+# Create a Network Interface.
+
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  count = var.vm_count
+
+  name                = "${var.prefix}-nic-${var.vm_names[count.index]}"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
 
   ip_configuration {
     name                          = "internal"
@@ -33,27 +86,79 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
+# Create a Public IP.
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-publicIp"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  allocation_method   = "Static"
+
+}
+# Create a Load Balancer. 
+
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-lb"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+
+}
+
+# Your load balancer will need a backend address pool 
+# and address pool association for the network interface and the load balancer.
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "BackEndAddressPool"
+}
+
+# Create a virtual machine availability set.
+
+resource "azurerm_availability_set" "main" {
+  name                        = "${var.prefix}-aset"
+  location                    = data.azurerm_resource_group.main.location
+  resource_group_name         = data.azurerm_resource_group.main.name
+  platform_fault_domain_count = 2
+}
+
+# Create virtual machines. Make sure you use the image you deployed using Packer!
+# Create managed disks for your virtual machines.
+# Ensure a variables file allows for customers to configure the number of virtual machines 
+# and the deployment at a minimum.
+
 resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm"
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
-  size                            = "Standard_D2s_v3"
+  count = var.vm_count
+
+  name                            = "${var.prefix}-vm-${var.vm_names[count.index]}"
+  resource_group_name             = data.azurerm_resource_group.main.name
+  location                        = data.azurerm_resource_group.main.location
+  size                            = "Standard_B1s"
   admin_username                  = "${var.username}"
   admin_password                  = "${var.password}"
   disable_password_authentication = false
   network_interface_ids = [
-    azurerm_network_interface.main.id,
+    azurerm_network_interface.main[count.index].id,
   ]
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+  availability_set_id = azurerm_availability_set.main.id
+  source_image_id     = var.packerImageId
 
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
+}
+
+resource "azurerm_managed_disk" "main" {
+  name                 = "${var.prefix}-md"
+  location             = data.azurerm_resource_group.main.location
+  resource_group_name  = data.azurerm_resource_group.main.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
+
 }
